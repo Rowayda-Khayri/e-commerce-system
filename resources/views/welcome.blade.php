898ef72b1f01848113a381b681440f858966cb1e
Rowayda Khayri <rowayda_khayri@yahoo.com>
2016-10-18 12:02:20 +0200

    add notification icon to admin view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,6 +34,9 @@
                 </button>
                 
             </div>
+            
+            
+            
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
@@ -54,7 +57,23 @@
                     </li>
                     
                 </ul>
+                
+                
+                <!--Notifications--> 
+
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="dropdown dropdown-notifications">
+                            <a href="#notifications-panel" class="dropdown-toggle">
+                              <i data-count="2" class="glyphicon glyphicon-bell notification-icon"></i>
+                            </a>
+                        </li>
+
+                    </ul>
+                </div> <!------ End notification -->
             </div>
+            
+            
             <!-- /.navbar-collapse -->
         </div>
         <!-- /.container -->
@@ -77,6 +96,7 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
+    
 </body>
 
 </html>
