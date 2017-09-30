13dfea34b77fd5165e97e28b097dd99fe90a620b
Rowayda Khayri <rowayda_khayri@yahoo.com>
2017-09-30 19:02:37 +0200

    changes in admin view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,17 @@
 @section('pageContent')
 
 <h1> Hello Admin </h1>
-<br><br><br><br>
+<br><br>
 
 <h3>U R logged in now and this is your JWT:</h3>
 <br><br>
-<h4> "{{$token}}"</h4>
+<h4 style="margin-right: 100px;font-size: 14px;"> "{{$token}}"</h4>
 <br><br>
-<h3> But I won't use it for now in your  routes :)  </h3>
+<h3> But I won't use it for now in your  routes ..   </h3>
+<h3> Now, u can manage categories, items, orders and customers .. </h3>
+<br><br><br><br>
+<h2>Have Fun  :) </h2>
+
 
 
     
