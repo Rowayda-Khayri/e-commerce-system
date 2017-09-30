5d245a2a3a3d3e3e39b500a9ceca81463a460278
Rowayda Khayri <rowayda_khayri@yahoo.com>
2017-09-30 18:22:21 +0200

    implement admin login method

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -55,10 +55,26 @@ class AuthenticateController extends Controller
          return view('auth.adminLogin');
     }
    
-   public function adminLogin(){
+   public function adminLogin(Request $request){
        
-       return "Hiiiiiiiii";
+         $credentials = $request->only('email', 'password');
+
+        try {
+            // verify the credentials and create a token for the user
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            // something went wrong
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        // if no errors are encountered we can return a JWT
+//        return response()->json(compact('token'));
+        return response("Hello Admin, U R logged in now and this is your token :" .\n .$token . \n ." but I won't use it for now in your  routes :) ");
+    }
+
        
-   }
+   
    
 }
