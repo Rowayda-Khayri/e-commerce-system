d505b419a5cab8e5be086f02dd87a192cf56c532
Rowayda Khayri <rowayda_khayri@yahoo.com>
2017-10-01 14:54:39 +0200

    make migration to rename sent_at to ordered at in orders table

---
+++ database/migrations/2017_10_01_124634_rename_sent_at_column_to_ordered_at.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameSentAtColumnToOrderedAt extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->renameColumn('sent_at', 'ordered_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            //
+        });
+    }
+}
